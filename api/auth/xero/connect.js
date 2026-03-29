bb9c359bed7a27b8dbb34fcfca354491328e2ff3
conmckay21 <connor_mckay777@hotmail.co.uk>
2026-03-29 16:31:59 +0100

    Create connect.js

---
+++ api/auth/xero/connect.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const clientId = process.env.XERO_CLIENT_ID;
+  const redirectUri = process.env.XERO_REDIRECT_URI;
+  const scope = 'openid profile email accounting.transactions accounting.contacts offline_access';
+  const state = req.query.userId || 'unknown';
+
+  const url = new URL('https://login.xero.com/identity/connect/authorize');
+  url.searchParams.set('response_type', 'code');
+  url.searchParams.set('client_id', clientId);
+  url.searchParams.set('redirect_uri', redirectUri);
+  url.searchParams.set('scope', scope);
+  url.searchParams.set('state', state);
+
+  res.redirect(url.toString());
+}
